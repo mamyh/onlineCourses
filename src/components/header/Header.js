f6c3f32dd48caebace3df01288fd378111a778d2
mamyh <likhmaon@gmail.com>
2021-10-05 10:22:30 +0600

    some interesting code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
                 <div>
-                    <NavLink className="navbar-brand" to='/'><img src={logo} width={50} height={50} /></NavLink>
+                    <NavLink className="navbar-brand" to='/'><img src={logo} width={50} height={50} alt="my logo" /></NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
--- src/components/tool/ToolImages.js
+++ src/components/tool/ToolImages.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import tool1 from '../../images/tool1.jpg';
-import tool2 from '../../images/tool2.jpg';
-import tool3 from '../../images/tool3.jpg';
-import tool4 from '../../images/tool4.jpg';
-import tool5 from '../../images/tool5.jpg';
-import tool6 from '../../images/tool6.jpg';
+
 
 import "./Tool.css";
 
 
 const ToolImages = () => {
+
     return (
         <div className="images">
-            <img src={tool1} alt="" />
-            <img src={tool2} alt="" />
-            <img src={tool3} alt="" />
-            <img src={tool4} alt="" />
-            <img src={tool5} alt="" />
-            <img src={tool6} alt="" />
+            {Array.from({ length: 6 }).map((_, idx) => <img src={require(`../../images/tool${idx + 1}.jpg`).default} alt="tool pictures" />)}
         </div>
     )
 }
